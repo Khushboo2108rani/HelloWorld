a1d9796cfa2a5dbcf7d2dee437b213b52dd782d2
Khushboo2108rani <58814796+Khushboo2108rani@users.noreply.github.com>
2020-06-16 20:24:49 +0530

    Update add.java

--- add.java
+++ add.java
@@ -4,7 +4,7 @@ public class add {
         
       int num1 = 10, num2 = 15, sum;
       sum = num1 + num2;
-      System.out.println("Num1 is: " + num1 + "and Num2 is :" + num2);
+      System.out.println("Num1 is: " + num1 + " " + "and Num2 is :" + num2);
       System.out.println("Sum of these numbers: "+sum);
    }
 }
